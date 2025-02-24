e1b9a699ac7c3ba5faf92d1aa2b9a81e5cdf6f62
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-24 14:26:26 +0100

    Initial commit

---
+++ src/main/java/org/example/fischerecap004/FischeRecap004Application.java
@@ -0,0 +1,15 @@
+package org.example.fischerecap004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FischeRecap004Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FischeRecap004Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fischerecap004/FischeRecap004ApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.fischerecap004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FischeRecap004ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
